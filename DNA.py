1e8c6e7186ca81a4953d890ba324715851653c15
Inozemtsev Alexander <a.inozemtsev@me.com>
2013-01-19 18:23:45 +0400

    Made DNA cool

--- DNA.py
+++ DNA.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python
-
-import fileinput
-
-def count(DNA):
-	dict = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
-	for char in DNA:
-	    if char in dict:
-		    dict[char] += 1
-	print dict['A'], dict['C'], dict['G'], dict['T']
-	
-for line in fileinput.input():
-    count(line)
+import sys
+ 
+dna = sys.stdin.read().strip()
+for i in 'ACGT':
+    print dna.count(i),
