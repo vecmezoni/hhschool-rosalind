8f8b3180d702012ec3eab478b139e1bbc051facf
Inozemtsev Alexander <a.inozemtsev@me.com>
2013-01-19 19:44:52 +0400

    Added beautiful solution for hamm problem

--- hamm.py
+++ hamm.py
@@ -3,8 +3,4 @@ import sys
  
 str1 = sys.stdin.readline().strip()
 str2 = sys.stdin.readline().strip()
-diffs = 0
-for ch1, ch2 in zip(str1, str2):
-    if ch1 != ch2:
-        diffs += 1
-print diffs
+print sum([a != b for a, b in zip(s1, s2)])
